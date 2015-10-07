9e191085780e5a6dcc52b070f8a93c149bebc831
mlopez34 <michshk@gmail.com>
2015-10-07 00:49:44 -0500

    Pattern recognition
    finished brutecollinear

---
+++ Pattern Recognition/BruteCollinearPoints.java
@@ -0,0 +1,108 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import edu.princeton.cs.algs4.StdDraw;
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.StdOut;
+
+public class BruteCollinearPoints {
+   private int N = 0;
+   private ArrayList<LineSegment> ls;
+   public BruteCollinearPoints(Point[] points)    // finds all line segments containing 4 points
+   {
+       if (points == null)
+       {
+           throw new java.lang.NullPointerException();
+       }
+       ls = new ArrayList();
+       for (int i = 0; i < points.length; i++)
+       {
+           for ( int j = i+1; j < points.length; j++)
+           {
+               for (int k = j+1; k < points.length; k++)
+               {
+                   if (points[i].slopeTo(points[j]) == points[i].slopeTo(points[k])){
+                       
+                       for (int l = k+1; l < points.length; l++)
+                       {
+                           if (points[i].slopeTo(points[k]) == points[i].slopeTo(points[l]))
+                           {
+                               
+                               //get the min and max pts in the line seg
+                               
+                               Point[] temp = new Point[] {points[i], points[j], points[k], points[l]};
+                               //StdOut.println(temp[0] + "->" + temp[1] + "->" + temp[2]+ "->" + temp[3] + " slope i to k " + 
+                                              //points[i].slopeTo(points[k]) + " slope i to l " + points[i].slopeTo(points[l]));
+                               Arrays.sort(temp);
+                               //StdOut.println("sorted " + temp[0] + "->" + temp[1] + "->" + temp[2]+ "->" + temp[3]);
+                               Point min = temp[0];
+                               Point max = temp[3];
+                               LineSegment line = new LineSegment(min, max);
+                               Boolean found = false;
+                               if (ls.size() == 0)
+                               {
+                                   ls.add(line);
+                                   N = N+1;
+                               }
+                               else{
+                                   for (LineSegment seg : ls)
+                                   {
+                                       if (seg.toString().equals(line.toString()))
+                                       {
+                                           found = true;
+                                           break;
+                                       }
+                                   }
+                                   if (!found)
+                                   {
+                                       ls.add(line);
+                                       N = N+1;
+                                   }
+                               }
+                               
+                           }
+                       }
+                   }
+               }
+           }
+       }
+       //System.out.println(N);
+   }
+   public int numberOfSegments()        // the number of line segments
+   {
+       return this.N;
+   }
+   public LineSegment[] segments()                // the line segments
+   {
+       LineSegment[] s = new LineSegment[N];
+       s = ls.toArray(s);
+       return s;
+   }
+   public static void main(String[] args) {
+
+       // read the N points from a file
+       In in = new In(args[0]);
+       int N = in.readInt();
+       Point[] points = new Point[N];
+       for (int i = 0; i < N; i++) {
+           int x = in.readInt();
+           int y = in.readInt();
+           points[i] = new Point(x, y);
+       }
+       
+       // draw the points
+       StdDraw.show(0);
+       StdDraw.setXscale(0, 32768);
+       StdDraw.setYscale(0, 32768);
+       for (Point p : points) {
+           p.draw();
+       }
+       StdDraw.show();
+       
+       // print and draw the line segments
+       BruteCollinearPoints collinear = new BruteCollinearPoints(points);
+       for (LineSegment segment : collinear.segments()) {
+           StdOut.println(segment);
+           segment.draw();
+       }
+   }
+}
---
+++ Pattern Recognition/Point.java
@@ -0,0 +1,100 @@
+import java.util.Comparator;
+import edu.princeton.cs.algs4.StdDraw;
+import edu.princeton.cs.algs4.StdOut;
+
+public class Point implements Comparable<Point> {
+   
+   private final int x;     // x-coordinate of this point
+   private final int y;     // y-coordinate of this point
+   
+   public Point(int x, int y) {
+        /* DO NOT MODIFY */
+        this.x = x;
+        this.y = y;
+    }
+   public void draw() {
+        /* DO NOT MODIFY */
+        StdDraw.point(x, y);
+    }                             // draws this point
+   public void drawTo(Point that) {
+        /* DO NOT MODIFY */
+        StdDraw.line(this.x, this.y, that.x, that.y);
+    }
+                  // draws the line segment from this point to that point
+   public String toString() {
+        /* DO NOT MODIFY */
+        return "(" + x + ", " + y + ")";
+    }                         // string representation
+
+   public int compareTo(Point that) 
+   {
+       // compare two points by y-coordinates, breaking ties by x-coordinates
+       if (this.y > that.y)
+       {
+           return 1;
+       }
+       else if (this.y < that.y)
+       {
+           return -1;
+       }
+       else if (this.y == that.y)
+       {
+           if (this.x > that.x)
+           {
+               return 1;
+           }
+           else if (this.x < that.x)
+           {
+               return -1;
+           }
+           else{
+               return 0;
+           }
+       }
+       return 0;
+       
+   }
+   public double slopeTo(Point that)
+   {
+       // the slope between this point and that point
+       if (this.x == that.x)
+       {
+           return +0;
+       }
+       if (this.y == that.y)
+       {
+           return Double.POSITIVE_INFINITY;
+       }
+       if (this.y == that.y && this.x == that.x)
+       {
+           return Double.NEGATIVE_INFINITY;
+       }
+       double slope = (double)(that.y - this.y)/(double)(that.x - this.x);
+       
+       return slope;
+   }
+   
+   public Comparator<Point> slopeOrder()
+   {
+       // compare two points by slopes they make with this point
+       return new SLOPEORDER();
+   }
+   private class SLOPEORDER implements  Comparator<Point>{
+       
+       public int compare(Point p1, Point p2)
+       {
+           Point p = new Point(x,y);
+           double d = p.slopeTo(p1) - p.slopeTo(p2);
+           if (d > 0)
+           {
+               return 1;
+           }
+           else if (d < 0)
+           {
+               return -1;
+           }
+           return 0;
+       }
+   }
+   
+}
